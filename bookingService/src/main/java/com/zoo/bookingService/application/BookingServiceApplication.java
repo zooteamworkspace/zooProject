27f6b2a823887beea3c68c5f35ce50d03d6dac77
nhatnguyen26 <nathan.nguyen2604@gmail.com>
2019-02-18 23:22:55 -0800

    add swagger for doc

--- bookingService/src/main/java/com/zoo/bookingService/application/BookingServiceApplication.java
+++ bookingService/src/main/java/com/zoo/bookingService/application/BookingServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.zoo.bookingService")
+@ComponentScan(basePackages = "com.zoo.bookingService.configuration")
 public class BookingServiceApplication {
 
 	public static void main(String[] args) {

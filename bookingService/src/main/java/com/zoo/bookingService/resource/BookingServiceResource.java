3cc0e9bc0629c65505e554302661d845a613057e
Koaha <ldvankhoa@gmail.com>
2019-03-25 02:08:16 +0800

    find booking by field ID with pagination

---
+++ bookingService/src/main/java/com/zoo/bookingService/resource/BookingServiceResource.java
@@ -0,0 +1,65 @@
+package com.zoo.bookingService.resource;
+
+import com.zoo.bookingService.response.FieldBooking;
+import com.zoo.bookingService.service.BookingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/v1/bookings")
+@Api(value = "Operations relating to field bookings and reservations")
+public class BookingResource {
+
+    @Inject
+    private BookingService bookingService;
+
+    @ApiOperation(value = "get current version", response = String.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Resource found")})
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/version")
+    public String getVersion() {
+        return "0.0.1";
+    }
+
+    @ApiOperation(value = "find the field bookings by id", response = FieldBooking.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Resource found")})
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{bookingId}")
+    public FieldBooking findById(@PathParam("bookingId") String bookingId) {
+        return bookingService.findBookingById(NumberUtils.toLong(bookingId));
+    }
+}
+
+@Path("")
+@Api(value = "Operations relating to field bookings and reservations")
+class BookingResourceService {
+
+    @Inject
+    private BookingService bookingService;
+
+    @ApiOperation(value = "find the field bookings by field id", response = FieldBooking.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Resource found")})
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("search?fieldId={fieldId}&limit={limit}&offset={offset}")
+    public List<FieldBooking> findByFieldId(@PathParam("fieldId") String fieldId,
+                                            @PathParam("limit") int lim,
+                                            @PathParam("offset") int offset) {
+        Pageable pageable = PageRequest.of(offset,lim);
+        return bookingService.findAllBookingByFieldId(NumberUtils.toLong(fieldId),pageable);
+    }
+}

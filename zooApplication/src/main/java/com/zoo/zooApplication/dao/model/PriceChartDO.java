5418f19bd1cc8bb3e49e63aac06d5458d2048989
Koaha <ldvankhoa@gmail.com>
2019-06-29 16:45:41 +0800

    Field Type

--- zooApplication/src/main/java/com/zoo/zooApplication/dao/model/PriceChartDO.java
+++ zooApplication/src/main/java/com/zoo/zooApplication/dao/model/PriceChartDO.java
@@ -19,6 +19,16 @@ import java.time.ZonedDateTime;
 @NoArgsConstructor
 public class PriceChartDO {
 
+    public enum DayOfWeek{
+        MON,
+        TUE,
+        WED,
+        THU,
+        FRI,
+        SAT,
+        SUN
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
